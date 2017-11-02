3ad333e40912dade456526d34724b48c3f0f6e75
Diego Martín <dmarvp@gmail.com>
2017-11-02 10:24:44 -0500

    Added tests
    Added tests using Jest + Virtual Alexa.

---
+++ test/index.test.js
@@ -0,0 +1,41 @@
+const virtualAlexa = require("virtual-alexa");
+const alexa = virtualAlexa.VirtualAlexa.Builder()
+    .handler("index.handler") // Lambda function file and name
+    .interactionModelFile("speechAssets/interactionModel.json")
+    .create();
+
+describe("My Pokedex Tests", () => {
+
+    test("Launches and asks for voltorb, then cancels  ", (done) => {
+        alexa.launch().then((payload) => {
+            expect(payload.response.outputSpeech.ssml).toContain("Ask me which number of pokemon you want to know about");
+            return alexa.utter("100");
+
+        }).then((payload) => {
+            expect(payload.response.outputSpeech.ssml).toContain("voltorb");
+            return alexa.utter("no");
+
+        }).then((payload) => {
+            expect(payload.response.outputSpeech.ssml).toContain("Thank you");
+            expect(payload.response.shouldEndSession).toBe(true);
+            done();
+
+        });
+    });
+
+    test("Launches and asks for charizard, then asks for the description  ", (done) => {
+        alexa.launch().then((payload) => {
+            expect(payload.response.outputSpeech.ssml).toContain("Ask me which number of pokemon you want to know about");
+            return alexa.utter("6");
+
+        }).then((payload) => {
+            expect(payload.response.outputSpeech.ssml).toContain("charizard");
+            return alexa.utter("ok");
+        }).then((payload) => {
+            expect(payload.response.outputSpeech.ssml).toContain("charizard");
+            expect(payload.response.outputSpeech.ssml).toContain("Thank you");
+            expect(payload.response.shouldEndSession).toBe(true);
+            done() ;  
+        });
+    });
+});
