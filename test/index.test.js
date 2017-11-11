cd143942890bb16fa0716d289966aac4c99911f6
Diego Martín <dmarvp@gmail.com>
2017-11-11 14:19:16 -0500

    Increased the timeout value for each test

--- test/index.test.js
+++ test/index.test.js
@@ -6,28 +6,35 @@ const alexa = virtualAlexa.VirtualAlexa.Builder()
 
 describe("My Pokedex Tests", () => {
 
-    test("Launches and asks for voltorb, then cancels  ", () => {
+    var originalTimeout;
+
+    beforeEach(function () {
+        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+    });
+
+    afterEach(function () {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    });
+
+    test("Launches and asks for voltorb, then cancels  ", (done) => {
         alexa.launch().then((payload) => {
             expect(payload.response.outputSpeech.ssml).toContain("Ask me which number of pokemon you want to know about");
             return alexa.utter("100");
-
         }).then((payload) => {
             expect(payload.response.outputSpeech.ssml).toContain("voltorb");
             return alexa.utter("no");
-
         }).then((payload) => {
             expect(payload.response.outputSpeech.ssml).toContain("Thank you");
             expect(payload.response.shouldEndSession).toBe(true);
             done();
-
         });
     });
 
-    test("Launches and asks for charizard, then asks for the description  ", () => {
+    test("Launches and asks for charizard, then asks for the description  ", (done) => {
         alexa.launch().then((payload) => {
             expect(payload.response.outputSpeech.ssml).toContain("Ask me which number of pokemon you want to know about");
             return alexa.utter("6");
-
         }).then((payload) => {
             expect(payload.response.outputSpeech.ssml).toContain("charizard");
             return alexa.utter("ok");
@@ -35,7 +42,7 @@ describe("My Pokedex Tests", () => {
             expect(payload.response.outputSpeech.ssml).toContain("charizard");
             expect(payload.response.outputSpeech.ssml).toContain("Thank you");
             expect(payload.response.shouldEndSession).toBe(true);
-            done() ;  
+            done();
         });
     });
 });
