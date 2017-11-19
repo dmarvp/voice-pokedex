548125f0cabf295be4d7d7b0f6a41fb126970baa
Diego Martín <dmarvp@hotmail.com>
2017-11-18 19:50:40 -0500

    Stop, Cancel and Help intents. Asking for more than one pokemon.
    - Added the default amazon intents: Stop, cancel and help.
    - Once the user declines to hear a pokemon's description, the skill asks if the user wants to hear about another pokemon.
    - Changed some of the dialogues strings.
    - Changed the tests accordingly.

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const webhook = require('jovo-framework').Webhook;
 const Pokedex = require('pokedex-promise-v2');
 
 // Listen for post requests (Web hook)
-/* webhook.listen(3000, function () {
-    console.log('Local development server listening on port 3000.');
-});
+// webhook.listen(3000, function () {
+//     console.log('Local development server listening on port 3000.');
+// });
 
-webhook.post('/webhook', function (req, res) {
-    app.handleRequest(req, res, handlers);
-    app.execute();
-}); */
+// webhook.post('/webhook', function (req, res) {
+//     app.handleRequest(req, res, handlers);
+//     app.execute();
+// });
 
 // Listen for post requests (Lambda)
 exports.handler = function (event, context, callback) {
@@ -30,15 +30,18 @@ exports.handler = function (event, context, callback) {
 // App Logic
 // =================================================================================
 
-const repromptMessage = 'Ask me which number of pokemon you want to know about';
-const errorMessage = 'There was an error with your request, please try again';
-const goodbyeMessage = 'Thank you for using my pokedex! Remember to catch them all!';
+const repromptMessage = "¿Which number of pokemon would you like to know about?";
+const errorMessage = "There was an error with your request, please try again";
+const goodbyeMessage = "Thank you for using the voice pokedex! Remember to catch them all!";
+const followupQuestion = "¿Would you like to ask for another pokemon?";
+const helpMessage = "Tell me the number of a pokemon you would like to know about." +
+    " Currently, you can ask for pokemons up to the number 802." + 
+    " If I find the pokemon you are looking for, I'll give you the option to hear one of it's pokedex description as well. " + repromptMessage;
 const pikachuAudio = 'https://s3.us-east-2.amazonaws.com/diego-bst-generalbucket/pikachu.mp3';
 
 const handlers = {
-
     'LAUNCH': function () {
-        const welcome = 'Welcome to my pokedex. ' + repromptMessage;
+        const welcome = 'Welcome to the voice pokedex. ' + repromptMessage;
         app.ask(welcome, repromptMessage);
     },
 
@@ -50,13 +53,13 @@ const handlers = {
                     //Save the pokemon number to a session variable
                     app.setSessionAttribute('pokemonNo', number);
                     app.setSessionAttribute('pokemonImg', response.sprites.front_default);
-                    const description = 'The pokemon at ' + number + ' is ' + response.name + '.';
-                    const reprompt = 'Do you want to hear ' + response.name + "'s description?";
+                    const description = `The pokemon at ${number} is ${response.name}. `;
+                    const reprompt = `Do you want to hear ${response.name}'s description?`;
 
                     let speech = "";
                     if (number == 25) {
                         speech = app.speechBuilder()
-                            .addText('The pokemon at ' + number + ' is ')
+                            .addText(`The pokemon at ${number} is `)
                             .addAudio(pikachuAudio, 'Pikachu')
                             .addText(reprompt);
                     }
@@ -78,9 +81,7 @@ const handlers = {
                 }
             });
     },
-
     'DescriptionState': {
-
         'YesIntent': function () {
             const P = new Pokedex();
             const number = app.getSessionAttribute('pokemonNo');
@@ -91,29 +92,33 @@ const handlers = {
                     const dexEntry = response.flavor_text_entries.filter(function (entry) {
                         return (entry.language.name === 'en');
                     });
-                    const description = dexEntry[getRandomInt(0, dexEntry.length - 1)].flavor_text;
-                    app.showImageCard(response.name, description, img).tell(response.name + ': ' + description + '. ' + goodbyeMessage);
+                    const description = dexEntry[getRandomInt(0, dexEntry.length - 1)].flavor_text.replace(/[\x00-\x1F\x7F-\x9F]/g, " ");
+                    app.followUpState('ContinueState').showImageCard(response.name, description, img).ask(`${response.name}: ${description} ${followupQuestion}`);
                 })
                 .catch(function (error) {
                     console.log(error);
                     app.tell(errorMessage);
                 });
         },
-
         'NoIntent': function () {
-            app.tell(goodbyeMessage);
+            app.followUpState('ContinueState').ask(followupQuestion);
         },
 
     },
-
-    'YesIntent': function () {
-        app.ask(repromptMessage, repromptMessage);
+    'ContinueState': {
+        'YesIntent': function () {
+            app.ask(repromptMessage);
+        },
+        'NoIntent': function () {
+            app.tell(goodbyeMessage);
+        },
     },
-
-    'NoIntent': function () {
+    'AMAZON.CancelIntent': function () {
         app.tell(goodbyeMessage);
     },
-
+    'AMAZON.HelpIntent': function () {
+        app.ask(helpMessage);
+    },
 };
 
 /* Returns a random integer between min (inclusive) and max (inclusive)
--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,7 @@ const alexa = virtualAlexa.VirtualAlexa.Builder()
     .interactionModelFile("speechAssets/interactionModel.json")
     .create();
 
-describe("My Pokedex Tests", () => {
+describe("Voice Pokedex Tests", () => {
 
     var originalTimeout;
 
@@ -19,7 +19,7 @@ describe("My Pokedex Tests", () => {
 
     test("Launches and asks for voltorb, then cancels  ", (done) => {
         alexa.launch().then((payload) => {
-            expect(payload.response.outputSpeech.ssml).toContain("Ask me which number of pokemon you want to know about");
+            expect(payload.response.outputSpeech.ssml).toContain("Which number of pokemon would you like to know about");
             return alexa.utter("100");
         }).then((payload) => {
             expect(payload.response.outputSpeech.ssml).toContain("voltorb");
@@ -33,15 +33,15 @@ describe("My Pokedex Tests", () => {
 
     test("Launches and asks for charizard, then asks for the description  ", (done) => {
         alexa.launch().then((payload) => {
-            expect(payload.response.outputSpeech.ssml).toContain("Ask me which number of pokemon you want to know about");
+            expect(payload.response.outputSpeech.ssml).toContain("Which number of pokemon would you like to know about");
             return alexa.utter("6");
         }).then((payload) => {
             expect(payload.response.outputSpeech.ssml).toContain("charizard");
             return alexa.utter("ok");
         }).then((payload) => {
             expect(payload.response.outputSpeech.ssml).toContain("charizard");
-            expect(payload.response.outputSpeech.ssml).toContain("Thank you");
-            expect(payload.response.shouldEndSession).toBe(true);
+            expect(payload.response.outputSpeech.ssml).toContain("Would you like to ask for another pokemon");
+            expect(payload.response.shouldEndSession).toBe(false);
             done();
         });
     });
